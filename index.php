0f9221d3a726b42fa6bddd0b98adaa92858fcac3
dpalaguta <d.palaguta@gmail.com>
2018-11-24 14:36:27 +0200

    Init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 15.11.2018
+ * Time: 19:13
+ */
+include_once ('RecursionTask.php');
+
+
+RecursionTask::class;
+$rt = new RecursionTask();
+echo $rt->count7(717);
+
